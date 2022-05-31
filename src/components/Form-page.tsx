e1f5571adc96eb5d4075fd05c18ec8228683a81d
Irene <iavivas3@gmail.com>
2022-05-31 18:01:24 +0200

    Create components

---
+++ src/components/Form-page.tsx
@@ -0,0 +1,11 @@
+export function FormPage() {
+  return (
+    <main>
+      <h1>Form</h1>
+      <h2>Personal Data</h2>
+      <h2>Access Data</h2>
+      <h2>Confirmation</h2>
+      <h2>Login</h2>
+    </main>
+  );
+}
